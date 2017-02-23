2d594fb8c1e9d578aec599aa3b91850e5327296a
Tim Joosten <Tjoosten3@gmail.com>
2017-02-23 05:29:41 +0100

    Add database schema for the auth system

--- database.sql
+++ database.sql
@@ -3470,6 +3470,63 @@ CREATE TABLE `users` (
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+DROP TABLE IF EXISTS permissions;
+/*!40101 SET @saved_cs_client = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE IF NOT EXISTS permissions (
+	PRIMARY KEY (id),
+	id	           INT			      AUTO_INCREMENT,
+	name           VARCHAR(255)       DEFAULT NULL,
+	description	   TEXT		          DEFAULT NULL,
+    updated_at     TIMESTAMP NULL     DEFAULT NULL,
+    created_at     TIMESTAMP NULL     DEFAULT NULL,
+    deleted_at     TIMESTAMP NULL     DEFAULT NULL
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS abilities;
+/*!40101 SET @saved_cs_client = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE IF NOT EXISTS abilities (
+	PRIMARY KEY (id),
+	id	           INT			      AUTO_INCREMENT,
+	name           VARCHAR(255)       DEFAULT NULL,
+	description	   TEXT		          DEFAULT NULL,
+    updated_at     TIMESTAMP NULL     DEFAULT NULL,
+    created_at     TIMESTAMP NULL     DEFAULT NULL,
+    deleted_at     TIMESTAMP NULL     DEFAULT NULL
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS login_permissions;
+/*!40101 SET @saved_cs_client = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE IF NOT EXISTS login_permissions (
+	PRIMARY KEY (id),
+    CONSTRAINT 	FK_permission_login FOREIGN KEY (login_id)      REFERENCES users(id),
+    CONSTRAINT 	FK_permission_data  FOREIGN KEY (permission_id) REFERENCES permissions(id),
+	id	           INT			    AUTO_INCREMENT,
+    login_id       INT              DEFAULT NULL,
+    permission_id  INT              DEFAULT NULL,
+    updated_at     TIMESTAMP NULL   DEFAULT NULL,
+    created_at     TIMESTAMP NULL   DEFAULT NULL
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS login_abilities;
+/*!40101 SET @saved_cs_client = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE IF NOT EXISTS login_abilities (
+    PRIMARY KEY (ID),
+    CONSTRAINT FK_ability_login FOREIGN KEY (login_id)   REFERENCES users(id),
+    CONSTRAINT FK_ability_data  FOREIGN KEY (ability_id) REFERENCES abilities(id),
+    id              INT              AUTO_INCREMENT,
+    login_id        INT              DEFAULT NULL,
+    ability_id      INT              DEFAULT NULL,
+    updated_at      TIMESTAMP NULL   DEFAULT NULL,
+    created_at      TIMESTAMP NULL   DEFAULT NULL
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
 --
 -- Dumping data for table `users`
 --
