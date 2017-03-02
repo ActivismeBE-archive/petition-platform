039133137957c9748cd1c61e2f20c7b7f8722424
Tim Joosten <Tjoosten3@gmail.com>
2017-03-02 23:54:32 +0100

    Update

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -3,7 +3,7 @@
 class Users extends MY_Controller
 {
     public $user        = []; /** */
-    public $abilities   = []; /** */
+    public $abilities   = []; /** @var array $abilities    The ablities for the authencated user. */
     public $permissions = []; /** @var array $permissions  The permissions for the authencated user.  */
 
     /**
@@ -14,12 +14,12 @@ class Users extends MY_Controller
     public function __construct()
     {
         parent::__construct();
-        $this->load->library():
-        $this->load->helpder();
+        $this->load->library(['session', 'blade', 'form_validation', 'pagination', 'paginator']);
+        $this->load->helper(['url']);
 
-        $this->user        =
-        $this->abilities   =
-        $this->permissions =
+        $this->user        = $this->session->userdata('user');
+        $this->abilities   = $this->session->userdata('abilities');
+        $this->permissions = $this->session->userdata('permissions');
     }
 
     /**
@@ -37,17 +37,82 @@ class Users extends MY_Controller
         return [];
     }
 
+    /**
+     * Get the backend index for the login management.
+     *
+     * @see:url('GYET|HEAD', 'http://www.petities.activisme.be/users')
+     * @return Blade view
+     */
     public function index()
     {
+        $data['title'] = 'Loginbeheer';
+        $data['db_users'] = Authencate::all();
 
+        // Users pagination.
+        $this->pagination->initialize($this->paginator->relation(base_url('users'), count($data['db_users']), 3, 3));
+        $data['users']      = $data['db_users']->skip($this->input->get('page'))->take(15)->get();
+        $data['users_link'] = $this->pagination->create_links();
+
+        return $this->blade->render('users/index', $data);
     }
 
-    public function status()
+    /**
+     * Search for a user in the system.
+     *
+     * @see:url('GET|HEAD', 'http://www.petities.activisme.be/users/search')
+     * @return Blade view
+     */
+    public function search()
     {
+        $term = $this->security->xss_clean($this->input->get('term'));
+
+        $data['title'] = 'Zoekresultaten voor' . $term;
+        $data['users'] = Authencate::where('name', 'LIKE', '%' . $term . '%')
+            ->orWhere('username', 'LIKE', '%' . $term . '%')
+            ->orWhere('email', 'LIKE', '%' . $term . '%')
+            ->get();
+
+        $this->blade->render('users/index', $data);
     }
 
+    /**
+     * Show a the specific user data for a account.
+     *
+     * @see:url('GET|HEAD, 'http://www.petities.activisme.be/users/show/{userId}')
+     * @return Response|Blade view
+     */
+    public function show()
+    {
+        $userId = $this->security->xss_clean($this->uri->segment(3));
+
+        $data['user']  = Authencate::find($id);
+        $data['title'] = 'Profiel: ' . $data['user']->name . '(' . $data['user']->username . ')';
+
+        if ((int) count($data['user']) === 0) {
+            $this->session->set_flashdata('class', 'alert alert-success');
+            $this->session->set_flashdata('message', 'Wij konden geen gebruiker vinden met de id #' . $userId);
+
+            return redirect($_SERVER['HTTP_REFERER']);
+        }
+
+        return $this->blade->render('users/show', $data);
+    }
+
+    /**
+     * Delete an user account on the system.
+     *
+     * @see:url('GET|HEAD', 'http://www.petities.activisme.be/users/delete/{userId}')
+     * @return Redirect|Response
+     */
     public function delete()
     {
+        $userId = $this->uri->segment(3);
 
+        if (Authencate::find($this->security->xss_clean($userId))) {
+            $this->session->set_flashdata('class', 'alert alert-success');
+            $this->session->set_flashdata('message', 'De gebruiker is verwijderd.');
+        }
+
+        return redirect($_SERVER['HTTP_REFERER']);
     }
 }
---
+++ application/views/users/index.blade.php
@@ -0,0 +1,66 @@
+@layout('layouts/app')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <form class="form-inline pull-left" action="{{ base_url('users/search') }}" method="post">
+                <div class="form-group">
+                    <label class="sr-only" for="term">Zoekterm:</label>
+                    <input type="text" class="form-control input-sm" id="term" placeholder="Uw zoekterm">
+                </div>
+
+                <button type="submit" class="btn btn-sm btn-info">
+                    <span class="fa fa-search" aria-hidden="true"></span> Zoek
+                </button>
+            </form>
+
+            <a class="pull-right btn btn-sm btn-primary" href="">
+                <span class="fa fa-plus" aria-hidden="true"></span> Gebruiker toevoegen
+            </a>
+        </div>
+    </div>
+
+    <div class="row row-padding">
+        <div class="col-sm-12">
+            <div class="panel panel-default">
+                <div class="panel-body box-shadow">
+                    @if ((int) count($users) === 0) {{-- No users found --}}
+                        <div class="alert alert-info" role="alert">
+                            <strong><span class="fa fa-info-circle" aria-hidden="true"></span> Info:</strong>
+                            Wij konden geen gebruikers vinden in het systeem.
+                        </div>
+                    @else {{-- Users found --}}
+                        <table class="table table-condensed table-hover">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Naam:</th>
+                                    <th>Gebruikersnaam:</th>
+                                    <th>Email:</th>
+                                    <th colspan="2">Aangemaakt op:</th> {{-- Colspan="2" needed for the functions.  --}}
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($users as $user)
+                                    <tr>
+                                        <td><code>#U{{ $user->id }}</code></td>
+                                        <td>{{ $user->name }}</td>
+                                        <td>{{ $user->username }}</td>
+                                        <td>{{ $user->email }}</td>
+                                        <td>{{ $user->created_at }}</td>
+
+                                        <td class="pull-right"> {{-- Functions --}}
+                                            <a href="{{ base_url() }}" class="label label-info">Bekijk</a>
+                                            <a href="{{ base_url() }}" class="label label-danger">Blokkeer</a>
+                                            <a href="{{ base_url() }}" class="label label-danger">Verwijder</a>
+                                        </td> {{-- /Functions --}}
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    @endif
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
